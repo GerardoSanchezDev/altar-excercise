a03b11c9827be9e4c2dc60ba577ef7a78ff01d9b
Alan Gerardo Sanchez Luna <gerardo.sanchez.dev@outlook.com>
2024-06-10 13:21:53 -0600

    grid generation

---
+++ frontend-app/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { GridComponent } from './components/grid/grid.component';
+
+const config: SocketIoConfig = { url: 'http://localhost:5000', options: {} };
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GridComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    NgbModule,
+    SocketIoModule.forRoot(config),
+    HttpClientModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
